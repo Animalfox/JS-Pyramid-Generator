6c134b5b2e1a8551bcb4bed080cd8a583505124d
Animalfox <a72e@outlook.com>
2024-07-07 15:23:55 +0300

    Use .push() to add the string "freeCodeCamp" to the end of rows array

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
  * @description
- * Step 25
- * Now you are ready to move onto the next set of array lessons.
+ * Step 26
+ * A method in JavaScript is a function that's associated with certain values or
+ * objects. An example you've already encountered is the .log() method, which is
+ * part of the console object.
  *
- * Remove all of your code from the previous step.
+ * Arrays have their own methods, and the first you will explore is the .push()
+ * method. This allows you to "push" a value to the end of an array.
+ * Here is an example to add the number 12 to the end of an array:
+ *
+ * Example Code
+ * array.push(12);
+ * Use .push() to add the string "freeCodeCamp" to the end of your rows array.
+ * Add this code before your console.log so you can see the change you made to
+ * your array.
  */
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
 console.log(rows);
