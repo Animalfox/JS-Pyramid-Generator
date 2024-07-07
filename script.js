1859b09382a953240152c479489e40154925ca70
Animalfox <a72e@outlook.com>
2024-07-07 15:21:58 +0300

    Remove all code from the previous step

--- script.js
+++ script.js
@@ -2,27 +2,12 @@
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
  * @description
- * In the last few steps, you learned all about working with arrays.
- * Take a moment to review what you have learned.
+ * Step 25
+ * Now you are ready to move onto the next set of array lessons.
  *
- * Start by declaring a cities variable and initializing it as an array of the
- * strings "London", "New York", and "Mumbai".
- * Then log that variable to the console.
- *
- * After logging, change the last element of cities to the string "Mexico City",
- * then log the cities variable again.
- *
- * When done correctly, you should see this output in the console.
- *
- * Example Code
- * [ "London", "New York", "Mumbai" ]
- * [ "London", "New York", "Mexico City" ]
+ * Remove all of your code from the previous step.
  */
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-let cities = ["London", "New York", "Mumbai"];
-console.log(cities);
-cities[cities.length - 1] = "Mexico City";
-console.log(cities);
 console.log(rows);
