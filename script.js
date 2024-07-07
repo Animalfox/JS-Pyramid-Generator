df669d1c16c54ed9e6d8b4ad7cd466a1b5b414f6
Animalfox <a72e@outlook.com>
2024-07-07 16:02:04 +0300

    Add a log statement to print the value of result

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 /**
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
- * @description Step 39
- * Unfortunately, now you cannot see what your loop is doing.
- *
- * Use let to declare a result variable, and assign it an empty string.
- * An empty string is represented by quotation marks with nothing between them,
- * such as "".
+ * @description Step 40
+ * Add a log statement to print the value of result.
+ * Depending on which console you use, you may not see anything printed.
  */
 const character = "#";
 const count = 8;
@@ -15,3 +12,4 @@ for (let i = 0; i < count; i = i + 1) {
     rows.push(i);
 }
 let result = "";
+console.log(result);
