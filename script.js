c6d2004249c69f54dbc1e6bce0672d8b86b91aef
Animalfox <a72e@outlook.com>
2024-07-07 13:46:03 +0300

    fix: typo

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ let character = 'Hello';
 console.log(character);
 character = "World";
 let secondCharacter;
-secondCharacter = characters;
+secondCharacter = character;
 console.log(secondCharacter);
