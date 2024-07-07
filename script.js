1b3ae8cda83aba56b3cf12ed5a83a8de26207d92
Animalfox <a72e@outlook.com>
2024-07-07 13:47:12 +0300

    You are now ready to declare your next variable. Remove both console.log statements, and the character reassignment. Also remove your secondCharacter variable, but leave the character initialization unchanged

--- script.js
+++ script.js
@@ -3,8 +3,3 @@
  * @author Animalfox <a72e@outlook.com>
  */
 let character = 'Hello';
-console.log(character);
-character = "World";
-let secondCharacter;
-secondCharacter = character;
-console.log(secondCharacter);
