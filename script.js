4cc13ef1c3fcd709f7cdb964e4f4e83a3ff6f919
Animalfox <a72e@outlook.com>
2024-07-07 15:35:19 +0300

    Create a new variable called popped and assign it the result of rows.pop()

--- script.js
+++ script.js
@@ -2,23 +2,20 @@
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
  * @description
- * Step 26
- * A method in JavaScript is a function that's associated with certain values or
- * objects. An example you've already encountered is the .log() method, which is
- * part of the console object.
+ * Step 27
+ * Another method essential for this project is the .pop() method.
+ * It removes the last element from an array and returns that element.
  *
- * Arrays have their own methods, and the first you will explore is the .push()
- * method. This allows you to "push" a value to the end of an array.
- * Here is an example to add the number 12 to the end of an array:
+ * When a method returns a value, you can think of it as giving the value back
+ * to you, making it available for use in other parts of your code.
  *
- * Example Code
- * array.push(12);
- * Use .push() to add the string "freeCodeCamp" to the end of your rows array.
- * Add this code before your console.log so you can see the change you made to
- * your array.
+ * Create a new variable called popped and assign it the result of rows.pop().
+ * Then, log popped to the console.
  */
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
 console.log(rows);
