1d54c97e24ce3f7bfa936717602c2e6f49bdf74a
Animalfox <a72e@outlook.com>
2024-07-07 13:33:44 +0300

    Use the let keyword to declare a variable called character

--- script.js
+++ script.js
@@ -2,9 +2,4 @@
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
  */
-console.log("Let's build a pyramid using JavaScript basics:");
-console.log("    #    ");
-console.log("   ###   ");
-console.log("  #####  ");
-console.log(" ####### ");
-console.log("#########");
+let character;
