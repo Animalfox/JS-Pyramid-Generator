c0feb5d57747b9f78ebef806e92a42c221e3c649
Animalfox <a72e@outlook.com>
2024-07-07 14:02:19 +0300

    Update the third element then print rows array

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
  * @description
- * You can access the values inside an array using the index of the value. An
- * index is a number representing the position of the value in the array,
- * starting from 0 for the first value.
+ * Arrays are special in that they are considered mutable. This means you can
+ * change the value at an index directly.
  *
- * You can access the value using bracket notation, such as array[0].
- * 
- * Use console.log and bracket notation to print the first value in your rows
- * array.
+ * For example, this code would assign the number 25 to the second element in
+ * the array:
+ *
+ * Example Code
+ * let array = [1, 2, 3];
+ * array[1] = 25;
+ * console.log(array); // prints [1, 25, 3]
+ *
+ * Update the third element of your rows array to be the number 10. Then print
+ * the rows array to your console.
  */
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 console.log(rows[0]);
+rows[2] = 10;
+console.log(rows);
