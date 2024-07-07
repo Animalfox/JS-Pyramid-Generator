984865595b9ad04b0ea1fec41ba7a5b397e2ebf9
Animalfox <a72e@outlook.com>
2024-07-07 13:57:26 +0300

    Change your rows declaration to be an array with the strings

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 /**
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
+ * @description
+ * When an array holds values, or elements, those values are separated by
+ * commas. Here is an array that holds two strings:
+ * 
+ * Example Code
+ * let array = ["first", "second"];
+ * 
+ * Change your rows declaration to be an array with the strings "Naomi",
+ * "Quincy", and "CamperChan". The order of values in an array is important, so
+ * follow that order. Remember that strings are case-sensitive.
  */
 let character = 'Hello';
 let count = 8;
-let rows = [];
+let rows = ["Naomi", "Quincy", "CamperChan"];
