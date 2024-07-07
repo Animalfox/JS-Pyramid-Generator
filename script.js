89cfc6660de54c5d40d80e8636166966f1e071fa
Animalfox <a72e@outlook.com>
2024-07-07 15:58:17 +0300

    Print for loop iteration

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 /**
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
- * @description Step 36
- * Your iteration statement will tell your loop what to do with the iterator
- * after each run.
- *
- * When you reassign a variable, you can use the variable to reference the
- * previous value before the reassignment. This allows you to do things like add
- * three to an existing number. For example, bees = bees + 3; would increase the
- * value of bees by three.
- *
- * Use that syntax to replace your "iteration" string with a reassignment
- * statement that increases i by one.
+ * @description Step 37
+ * Your loop should now run eight times.
+ * Inside the body of the loop, print the value of the i iterator and see what
+ * happens.
  */
 const character = "#";
 const count = 8;
 const rows = [];
 for (let i = 0; i < count; i = i + 1) {
-
+  console.log(i);
 }
