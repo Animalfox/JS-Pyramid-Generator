962e670067f120bd79fc7ffbb182d5f7dc5743d4
Animalfox <a72e@outlook.com>
2024-07-07 15:45:26 +0300

    Change the value of character to be the hash character "#"

--- script.js
+++ script.js
@@ -2,28 +2,13 @@
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
  * @description
- * Step 31
- * Declaring a variable with the let keyword allows it to be reassigned.
- * This means you could change character later to be a completely different
- * value.
+ * Step 32
+ * You are now ready to start building your pyramid generator.
+ * Your character variable will serve as the building block for the pyramid.
  *
- * For this project, you will not want to change these variable values.
- * So instead, you should use const to declare them.
- * const variables are special.
- *
- * First, a const variable cannot be reassigned like a let variable.
- * This code would throw an error:
- *
- * Example Code
- * const firstName = "Naomi";
- * firstName = "Jessica";
- * A const variable also cannot be uninitialized.
- * This code would throw an error:
- *
- * Example Code
- * const firstName;
- * Replace your let keywords with const
+ * "Hello" might not work very well for that. Change the value of character to
+ * be the hash character "#".
  */
-const character = "Hello";
+const character = "#";
 const count = 8;
 const rows = [];
