2d3a3c4e14a5af962eaf9f0d36bc83355e8a8853
Animalfox <a72e@outlook.com>
2024-07-07 16:32:05 +0300

    Push character instead i

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 /**
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
- * @description Step 43
- * Now all of your numbers are appearing on the same line.
- * This will not work for creating a pyramid.
+ * @description Step 44
+ * Printing numbers won't result in a visually appealing pyramid.
+ * Now that you're outputting the formatted content of your rows array,
+ * it's time to update your original loop.
  *
- * You will need to add a new line to each row.
- * However, pressing the return key to insert a line break between quotes in
- * JavaScript will result in a parsing error.
- * Instead, you need to use the special escape sequence \n, which is interpreted
- * as a new line when the string is logged. For example:
- *
- * Example Code
- * lineOne = lineOne + "\n" + lineTwo;
- *
- * Use a second addition operator to concatenate a new line between the existing
- * result value and the added row value.
+ * Instead of pushing i to the array, push the value of your character variable.
  */
 const character = "#";
 const count = 8;
 const rows = [];
+
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(i);
+  rows.push(character);
 }
+
 let result = "";
 for (const row of rows) {
   result = result + "\n" + row;
