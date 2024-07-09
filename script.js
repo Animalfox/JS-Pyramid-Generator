6329b17bb17a5eb94ddd50c1f997def9d0cb7417
Animalfox <a72e@outlook.com>
2024-07-09 17:42:53 +0300

    Call padRow function

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 /**
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
- * @description Step 47
- * The logic for formatting this pyramid is likely going to get complicated,
- * which means it's a great time to extract that code into a function.
- *
- * A function is a block of code that can be reused throughout your application.
- * Functions are declared with the following syntax:
- *
+ * @description 48
+ * In order to use a function, you need to call it. A function call tells your
+ * application to run the code from the function wherever you choose to call it.
+ * The syntax for a function call is the function name followed by parentheses.
+ * For example, this code defines and calls a test function.
+ * 
  * Example Code
- * function name(parameter) {
- *
+ * function test() {
+ * 
  * }
- *
- * The function keyword tells JavaScript that the name variable is going to be a
- * function. parameter is a variable that represents a value that is passed into
- * the function when it is used. A function may have as many, or as few,
- * parameters as you'd like. Like a for loop, the space between the curly braces
- * is the function body.
- *
- * Declare a padRow function. Do not create any parameter variables yet.
- * The function body should be empty. Remember that you need to use camel case
- * for your naming convention.
+ * test();
+ * 
+ * Call your padRow function.
  */
 const character = "#";
 const count = 8;
 const rows = [];
 
 function padRow() {}
+padRow();
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1));
