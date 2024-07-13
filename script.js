3667e1a15b2670c60176a2c10e19ae0e2797f03c
Animalfox <a72e@outlook.com>
2024-07-13 22:12:47 +0300

    Use the return keyword to have function return the string

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 /**
  * @file Main file for node project.
  * @author Animalfox <a72e@outlook.com>
- * @description Step 50
- * Now add a log statement to print the value of your call variable.
+ * @description Step 51
+ * Your call variable has an undefined value, even though you defined it!
+ * This is because your padRow function does not currently return a value.
+ * By default, functions return undefined as their value.
+ *
+ * In order to return something else, you need to use the return keyword.
+ * Here is an example of a function that returns the string "Functions are
+ * cool!":
+ *
+ * Example Code
+ * function demo() {
+ *   return "Functions are cool!";
+ * }
+ *
+ * Use the return keyword to have your function return the string "Hello!".
  */
 const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {}
+function padRow() {
+  return "Hello!";
+}
 const call = padRow();
 console.log(call);
 
